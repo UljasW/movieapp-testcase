a1c626ca10e5fc2eceea4c52da328b0e26787899
Uljas Wennstrom <uljas.wennstrom@gmail.com>
2023-11-05 12:02:07 +0100

    renamed

--- app/ui/sections/results/resultSection.tsx
+++ app/ui/sections/results/resultSection.tsx
@@ -4,7 +4,7 @@ import { useSearchParams } from "next/navigation";
 import React, { useEffect } from "react";
 import SmallMovieDisplay from "../../components/smallMovieDisplay";
 import { Movie } from "@/app/lib/types";
-import { getSearchAction } from "@/app/lib/getSearchAction";
+import { getSearchAction } from "@/app/lib/serverActions";
 
 export default function ResultSection() {
   const searchParams = useSearchParams();
