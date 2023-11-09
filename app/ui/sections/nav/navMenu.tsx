d3b511544093beb5c0008ffffea4747d99b2b854
Uljas Wennstrom <uljas.wennstrom@gmail.com>
2023-11-09 17:52:26 +0100

    done

--- app/ui/sections/nav/navMenu.tsx
+++ app/ui/sections/nav/navMenu.tsx
@@ -3,7 +3,7 @@ import NavButtons from "../../components/navButtons";
 
 export default function NavMenu() {
   return (
-    <nav className="bg-nav absolute h-full w-5/6 top-0 left-0 z-50 p-10">
+    <nav className="bg-nav fixed h-full w-5/6 top-0 left-0 z-50 p-10">
       <div className="mt-20">
         <NavButtons />
       </div>
