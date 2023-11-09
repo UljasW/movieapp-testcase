8352dc27164f12d3647de3e03c366767ad0d338a
Uljas Wennstrom <uljas.wennstrom@gmail.com>
2023-11-09 18:24:43 +0100

    small fix

--- app/ui/sections/nav/navBar.tsx
+++ app/ui/sections/nav/navBar.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import NavButtons from "../../components/navButtons";
 export default function Navbar() {
   return (
-    <nav className="w-72 p-10 shadow-custom hidden md:block bg-nav">
+    <nav className="w-72 min-h-screen	p-10 shadow-custom hidden md:block bg-nav">
       <div className="flex justify-center items-center">
         <h2 className="text-white text-2xl font-semibold">Movies</h2>
       </div>
